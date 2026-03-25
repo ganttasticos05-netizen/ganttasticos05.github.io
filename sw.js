9cc3531d27211066fe7cf002d7cc59fa13cebd5a
vanessitaram <vaneramz12@gmail.com>
2026-03-25 03:17:15 -0600

    prueba

--- sw.js
+++ sw.js
@@ -1,96 +1,81 @@
-const VERSION = "1.15"
-const CACHE = "pwamd"
+const VERSION = "1.16"
+const CACHE = "pwamd-v2"
 
 const ARCHIVOS = [
- "ayuda.html",
- "favicon.ico",
- "index.html",
- "site.webmanifest",
- "css/baseline.css",
- "css/colors.css",
- "css/elevation.css",
- "css/estilos.css",
- "css/material-symbols-outlined.css",
- "css/md-headline.css",
- "css/md-list.css",
- "css/md-tab.css",
- "css/motion.css",
- "css/palette.css",
- "css/roboto.css",
- "css/shape.css",
- "css/state.css",
- "css/transicion_pestanas.css",
- "css/typography.css",
- "css/theme/dark.css",
- "css/theme/light.css",
- "fonts/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].codepoints",
- "fonts/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].ttf",
- "fonts/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].woff2",
- "fonts/roboto-v32-latin-regular.woff2",
- "img/BALTA.png",
- "img/HECTOR.png",
- "img/icon-192.png",
- "img/icon-512-maskable.png",
- "img/icono2048.png",
- "img/ITATI.png",
- "img/maskable_icon_x48.png",
- "img/maskable_icon_x72.png",
- "img/maskable_icon_x96.png",
- "img/maskable_icon_x128.png",
- "img/maskable_icon_x192.png",
- "img/maskable_icon_x384.png",
- "img/maskable_icon_x512.png",
- "img/maskable_icon.png",
- "img/MENDIETA.png",
- "img/ROBER.png",
- "img/screenshot_horizontal.png",
- "img/screenshot_vertical.png",
- "img/vanne.png",
- "js/nav-tab-fixed.js",
+  "index.html",
+  "ayuda.html",
+  "site.webmanifest",
+  "favicon.ico",
 
- "js/lib/getAttribute.js",
- "js/lib/querySelector.js",
- "js/lib/registraServiceWorker.js",
- "js/lib/resaltaSiEstasEn.js",
+  "css/estilos.css",
+  "css/transicion_pestanas.css",
+  "css/md-tab.css",
+  "css/material-symbols-outlined.css",
+  "css/md-list.css",
+  "css/theme/light.css",
+  "css/theme/dark.css",
 
- "ungap/custom-elements.js",
- "/"
+  "img/BALTA.png",
+  "img/HECTOR.png",
+  "img/ITATI.png",
+  "img/MENDIETA.png",
+  "img/ROBER.png",
+  "img/Vanne.png",
+
+  "img/maskable_icon_x192.png",
+  "img/maskable_icon_x512.png",
+  "img/icon-192.png",
+  "img/icon-512-maskable.png",
+
+  "img/screenshot_horizontal.png",
+  "img/screenshot_vertical.png",
+
+  "js/nav-tab-fixed.js",
+  "js/lib/getAttribute.js",
+  "js/lib/querySelector.js",
+  "js/lib/resaltaSiEstasEn.js"
 ]
 
-if (self instanceof ServiceWorkerGlobalScope) {
- self.addEventListener("install", (evt) => {
+self.addEventListener("install", (evt) => {
+  console.log("SW instalado v" + VERSION)
+
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return Promise.allSettled(
+        ARCHIVOS.map(url =>
+          cache.add(url).catch(() =>
+            console.log("No cacheado:", url)
+          )
+        )
+      )
+    })
+  )
+
   self.skipWaiting()
-  evt.waitUntil(llenaElCache())
- })
+})
 
- self.addEventListener("fetch", (evt) => {
-  if (evt.request.method === "GET") {
-   evt.respondWith(buscaLaRespuestaEnElCache(evt))
-  }
- })
+self.addEventListener("activate", (evt) => {
+  console.log("SW activado")
 
- self.addEventListener("activate", (evt) => {
-  evt.waitUntil(self.clients.claim())
- })
-}
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  )
 
-async function llenaElCache() {
- const keys = await caches.keys()
- for (const key of keys) {
-  await caches.delete(key)
- }
- const cache = await caches.open(CACHE)
- for (const archivo of ARCHIVOS) {
-  try {
-   await cache.add(archivo)
-  } catch (e) {
-   console.error("Error cacheando:", archivo)
-  }
- }
-}
+  self.clients.claim()
+})
 
-async function buscaLaRespuestaEnElCache(evt) {
- const cache = await caches.open(CACHE)
- const response = await cache.match(evt.request, { ignoreSearch: true })
- return response || fetch(evt.request)
-}
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.method !== "GET") return
+
+  evt.respondWith(
+    caches.match(evt.request).then(res => {
+      return res || fetch(evt.request)
+    })
+  )
+})
