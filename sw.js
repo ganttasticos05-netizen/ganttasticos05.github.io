4e73e346754623fa6ba848a91bf73f1df419d7e5
vanessitaram <vaneramz12@gmail.com>
2026-03-25 03:29:13 -0600

    3

--- sw.js
+++ sw.js
@@ -1,81 +1,103 @@
-const VERSION = "1.16"
-const CACHE = "pwamd-v2"
+const VERSION = "1.15"
+const CACHE = "pwamd"
 
 const ARCHIVOS = [
-  "index.html",
-  "ayuda.html",
-  "site.webmanifest",
-  "favicon.ico",
+ "ayuda.html",
+ "favicon.ico",
+ "index.html",
+ "site.webmanifest",
 
-  "css/estilos.css",
-  "css/transicion_pestanas.css",
-  "css/md-tab.css",
-  "css/material-symbols-outlined.css",
-  "css/md-list.css",
-  "css/theme/light.css",
-  "css/theme/dark.css",
+ "css/baseline.css",
+ "css/colors.css",
+ "css/elevation.css",
+ "css/estilos.css",
+ "css/material-symbols-outlined.css",
+ "css/md-headline.css",
+ "css/md-list.css",
+ "css/md-tab.css",
+ "css/motion.css",
+ "css/palette.css",
+ "css/roboto.css",
+ "css/shape.css",
+ "css/state.css",
+ "css/transicion_pestanas.css",
+ "css/typography.css",
+ "css/theme/dark.css",
+ "css/theme/light.css",
 
-  "img/BALTA.png",
-  "img/HECTOR.png",
-  "img/ITATI.png",
-  "img/MENDIETA.png",
-  "img/ROBER.png",
-  "img/Vanne.png",
+ "fonts/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].codepoints",
+ "fonts/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].ttf",
+ "fonts/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].woff2",
+ "fonts/roboto-v32-latin-regular.woff2",
 
-  "img/maskable_icon_x192.png",
-  "img/maskable_icon_x512.png",
-  "img/maskable_icon_x192.png",
-  "img/maskable_icon-512.png",
+ // IMÁGENES (CORREGIDAS)
+ "img/BALTA.png",
+ "img/HECTOR.png",
+ "img/icon-192.png",
+ "img/icon-512-maskable.png",
+ "img/maskable_icon2048.png",  
+ "img/ITATI.png",
+ "img/maskable_icon_x48.png",
+ "img/maskable_icon_x72.png",
+ "img/maskable_icon_x96.png",
+ "img/maskable_icon_x128.png",
+ "img/maskable_icon_x192.png",
+ "img/maskable_icon_x384.png",
+ "img/maskable_icon_x512.png",
+ "img/maskable_icon.png",
+ "img/MENDIETA.png",
+ "img/ROBER.png",
+ "img/screenshot_horizontal.png",
+ "img/screenshot_vertical.png",
+ "img/Vanne.png",             
 
-  "img/screenshot_horizontal.png",
-  "img/screenshot_vertical.png",
+ "js/nav-tab-fixed.js",
+ "js/lib/getAttribute.js",
+ "js/lib/querySelector.js",
+ "js/lib/registraServiceWorker.js",
+ "js/lib/resaltaSiEstasEn.js",
 
-  "js/nav-tab-fixed.js",
-  "js/lib/getAttribute.js",
-  "js/lib/querySelector.js",
-  "js/lib/resaltaSiEstasEn.js"
+ "ungap/custom-elements.js"
+
 ]
 
-self.addEventListener("install", (evt) => {
-  console.log("SW instalado v" + VERSION)
+if (self instanceof ServiceWorkerGlobalScope) {
 
-  evt.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return Promise.allSettled(
-        ARCHIVOS.map(url =>
-          cache.add(url).catch(() =>
-            console.log("No cacheado:", url)
-          )
-        )
-      )
-    })
-  )
+ self.addEventListener("install", (evt) => {
+  evt.waitUntil(llenaElCache())
+ })
 
-  self.skipWaiting()
-})
+ self.addEventListener("fetch", (evt) => {
+  if (evt.request.method === "GET") {
+   evt.respondWith(buscaLaRespuestaEnElCache(evt))
+  }
+ })
 
-self.addEventListener("activate", (evt) => {
-  console.log("SW activado")
+ self.addEventListener("activate", (evt) => {
+  evt.waitUntil(self.clients.claim())
+ })
+}
 
-  evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    )
-  )
+async function llenaElCache() {
 
-  self.clients.claim()
-})
+ const keys = await caches.keys()
+ for (const key of keys) {
+  await caches.delete(key)
+ }
 
-self.addEventListener("fetch", (evt) => {
-  if (evt.request.method !== "GET") return
+ const cache = await caches.open(CACHE)
 
-  evt.respondWith(
-    caches.match(evt.request).then(res => {
-      return res || fetch(evt.request)
-    })
-  )
-})
+ for (const archivo of ARCHIVOS) {
+  try {
+   await cache.add(archivo)
+  } catch (e) {
+   console.error("Error cacheando:", archivo)
+  }
+ }
+}
+
+async function buscaLaRespuestaEnElCache(evt) {
+ const cache = await caches.open(CACHE)
+ const response = await cache.match(evt.request, { ignoreSearch: true })
+ return response || fetch(evt.request)
+}
